676b6462baed3eb7a87adeb18a8e38ddff047564
JvSlice <outeredgeforge@gmail.com>
2026-03-23 07:04:41 -0500

    Update enemy.dart
    1st save of transitions

--- lib/models/enemy.dart
+++ lib/models/enemy.dart
@@ -1,1 +1,30 @@
 
+import 'package:flutter/material.dart';
+import 'game_types.dart';
+
+class Enemy {
+  Enemy({
+    required this.x,
+    required this.distance,
+    required this.speed,
+    required this.tint,
+    required this.type,
+    required this.radiusScale,
+    required this.health,
+    required this.maxHealth,
+    this.weave = 0.0,
+  });
+
+  double x;
+  double distance;
+  double speed;
+  Color tint;
+  EnemyType type;
+  double radiusScale;
+  int health;
+  int maxHealth;
+  double weave;
+
+  bool alive = true;
+  double flash = 0.0;
+}
