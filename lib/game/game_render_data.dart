28ea47f72be5ca2786ee4000ddb855834e6716f0
JvSlice <outeredgeforge@gmail.com>
2026-04-13 06:08:01 -0500

    Update game_render_data.dart
    1st add

--- lib/game/game_render_data.dart
+++ lib/game/game_render_data.dart
@@ -1,1 +1,57 @@
-//place holder(unitWidth + c
+import 'dart:ui';
+
+import '../models/enemy.dart';
+import '../models/enemy_projectile.dart';
+import '../models/game_types.dart';
+import '../models/shot_trace.dart';
+
+class GameRenderData {
+  const GameRenderData({
+    required this.size,
+    required this.enemies,
+    required this.enemyProjectiles,
+    required this.traces,
+    required this.playerX,
+    required this.bobTime,
+    required this.worldZ,
+    required this.state,
+    required this.currentWave,
+    required this.firePressed,
+    required this.bossHealth,
+    required this.bossMaxHealth,
+    required this.worldXToScreen,
+    required this.enemyScreenY,
+    required this.enemyRadius,
+    required this.crosshairPosition,
+    required this.isTraveling,
+    required this.travelProgress,
+    required this.travelTurn,
+  });
+
+  final Size size;
+  final List<Enemy> enemies;
+  final List<EnemyProjectile> enemyProjectiles;
+  final List<ShotTrace> traces;
+
+  final double playerX;
+  final double bobTime;
+  final double worldZ;
+
+  final GameState state;
+  final int currentWave;
+  final bool firePressed;
+
+  final int bossHealth;
+  final int bossMaxHealth;
+
+  final Offset crosshairPosition;
+
+  final bool isTraveling;
+  final double travelProgress;
+  final double travelTurn;
+
+  final double Function(double worldX, double distance, Size size)
+      worldXToScreen;
+  final double Function(double distance, Size size) enemyScreenY;
+  final double Function(double distance) enemyRadius;
+}
