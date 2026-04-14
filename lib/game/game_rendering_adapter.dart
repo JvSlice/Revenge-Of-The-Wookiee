a8fef68ce85f1d1cd137d89a59fac0e5c83504ce
JvSlice <outeredgeforge@gmail.com>
2026-04-14 05:05:43 -0500

    Update game_rendering_adapter.dart
    refactor code

--- lib/game/game_rendering_adapter.dart
+++ lib/game/game_rendering_adapter.dart
@@ -1,1 +1,40 @@
-// place holder
+part of 'game_page.dart';
+
+extension _GamePageRenderingAdapter on _GamePageState {
+  void _handleLayoutChange(Size size, bool isLandscape) {
+    if (_lastLayoutSize != size || _lastLandscape != isLandscape) {
+      moveStick.stop();
+      aimStick.stop();
+      firePressed = false;
+      _lastLayoutSize = size;
+      _lastLandscape = isLandscape;
+    }
+  }
+
+  Widget _buildGamePainter(Size size) {
+    return CustomPaint(
+      size: size,
+      painter: RedwoodPainter(
+        size: size,
+        enemies: enemies,
+        enemyProjectiles: enemyProjectiles,
+        traces: traces,
+        playerX: playerX,
+        bobTime: bobTime,
+        worldZ: worldZ,
+        state: state,
+        currentWave: currentWave,
+        firePressed: firePressed,
+        bossHealth: _currentBossHealth,
+        bossMaxHealth: _currentBossMaxHealth,
+        worldXToScreen: _worldXToScreen,
+        enemyScreenY: _enemyScreenY,
+        enemyRadius: _enemyRadius,
+        crosshairPosition: _crosshairScreenPosition(size),
+        isTraveling: false,
+        travelProgress: 0.0,
+        travelTurn: 0.0,
+      ),
+    );
+  }
+}
